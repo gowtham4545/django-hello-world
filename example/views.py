2895a58b60f894fc25ff6b8b7be16b2fc0ebc97c
gowtham4545 <gowthamgopi444@gmail.com>
2023-12-20 22:52:21 +0530

    livechat token

--- example/views.py
+++ example/views.py
@@ -26,9 +26,9 @@ def index(request):
 
 
 def generate(req):
-    appId = '4f5f1bf38e0c440f86d3b1df203195c8'
-    appCertificate = '9e95450e6f7749b18cb30af425c20f90'
-    channelName = req.GET.get('channelName')
+    appId = os.environ.get('appId')
+    appCertificate = os.environ.get('appCertificate')
+    channelName = 'channleName'
     token = RtcTokenBuilder.buildTokenWithUid(
         appId, appCertificate, channelName, 1, 1, 3600)
     res = {'token': token}
